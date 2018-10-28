c174f5b2df9c149c64ea522de6ed9ad77ac0e624
Christian Hamamdjian <christian.hamamdjian@welho.com>
2018-10-29 00:01:30 +0200

    React page layout

--- src/RecentWorkCardComponent.js
+++ src/RecentWorkCardComponent.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
 
 class RecentWorkCardComponent extends React.Component {
-    render() {
-      return (
-        <div className="App-recent-work-card">
-        <img src={this.props.image} />
+  render() {
+    return (
+      <div className="App-recent-work-card">
+        <img alt="" src={this.props.image} />
         <h2>{this.props.title}</h2>
         <p>{this.props.content}</p>
-        <div className="recent-work-triangle-white"></div>
-        </div>
-      );
-    }
+        <div className="recent-work-triangle-white" />
+      </div>
+    );
   }
+}
 
-  export default RecentWorkCardComponent;
+export default RecentWorkCardComponent;
