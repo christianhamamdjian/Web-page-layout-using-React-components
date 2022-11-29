9953b1d092568ecae2277a12236c0b61fd806e50
Christian Hamamdjian <christian.hamamdjian@welho.com>
2022-11-29 18:37:08 +0200

    Deploy React app to GitHub Pages

---
+++ precache-manifest.703bb36ece7c5db6cf5bb3c5796b47ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0412d71e35e42993f3a",
+    "url": "/Web-page-layout-using-React-components/static/css/main.fd2a6cf7.chunk.css"
+  },
+  {
+    "revision": "a0412d71e35e42993f3a",
+    "url": "/Web-page-layout-using-React-components/static/js/main.a0412d71.chunk.js"
+  },
+  {
+    "revision": "9ec752698880d1bd31d2",
+    "url": "/Web-page-layout-using-React-components/static/js/1.9ec75269.chunk.js"
+  },
+  {
+    "revision": "e269a5cd1eb4d6bc887f",
+    "url": "/Web-page-layout-using-React-components/static/js/runtime~main.e269a5cd.js"
+  },
+  {
+    "revision": "a9e24e1b95fa43a3f93aa3aa8315a988",
+    "url": "/Web-page-layout-using-React-components/static/media/img-placeholder.a9e24e1b.png"
+  },
+  {
+    "revision": "e15941b94fb34ad1d6854b540cb95a92",
+    "url": "/Web-page-layout-using-React-components/static/media/Header-img.e15941b9.png"
+  },
+  {
+    "revision": "f870137e139cd0ed17ec3ed9058e5f8e",
+    "url": "/Web-page-layout-using-React-components/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Web-page-layout-using-React-components/precache-manifest.703bb36ece7c5db6cf5bb3c5796b47ca.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Web-page-layout-using-React-components/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
